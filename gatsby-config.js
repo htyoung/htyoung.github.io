7208a6c32dfc2722272f03cab9415ed8f015c2f5
tongyang.hty <hantongyang@xiaomi.com>
2025-07-17 14:31:18 +0800

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,5 +121,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "GA-G-5QZBV40Y6K", // Google Analytics
+        ],
+      }
+    }
   ],
 }
